5de014fc979e56396433095e09fd88f8120fd067
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-07-30 22:23:04 -0300

    rotas ok

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = 3000;
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+
+// config
+app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// default middleware para major e minor
+const isEmptyName = (req, res, next) => {
+  if (req.query.name === undefined) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
 
 app.get('/', (req, res) => {
-  res.send('Jesus loves you!');
+  res.render('main');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://...:${PORT}`);
+app.get('/major', isEmptyName, (req, res) => {
+  res.render('major', { name: req.query.name });
 });
+
+app.get('/minor', isEmptyName, (req, res) => {
+  res.render('minor', { name: req.query.name });
+});
+
+app.post('/check', (req, res) => {
+  const { name, date } = req.body;
+
+  res.redirect(`/major?name=${name}`);
+});
+
+app.listen(3000);
